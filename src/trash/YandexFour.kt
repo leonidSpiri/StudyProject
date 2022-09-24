8fcb77ffcf604183a1b46c54d2dbbf64945eb2f2
leonidSpiri <2003.leonid2003@gmail.com>
2022-09-25 00:35:56 +0300

    cool shit

---
+++ src/trash/YandexFour.kt
@@ -0,0 +1,30 @@
+package trash
+
+import java.util.*
+import kotlin.math.max
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val wArray = mutableListOf<Int>()
+    val hArray = mutableListOf<Int>()
+    val lArray = mutableListOf<Int>()
+    for (i in 0 until n) {
+        scanner.next()
+        wArray.add(scanner.nextInt())
+        hArray.add(scanner.nextInt())
+        lArray.add(scanner.nextInt())
+    }
+    wArray.sort()
+    hArray.sort()
+    lArray.sort()
+    val middle = if (n % 2 == 0) n / 2 - 1 else n / 2
+    val wMiddle = wArray[middle]
+    val hMiddle = hArray[middle]
+    val lMiddle = lArray[middle]
+    var result = Int.MIN_VALUE
+    result = max(result, wMiddle)
+    result = max(result, hMiddle)
+    result = max(result, lMiddle)
+    println(result)
+}
