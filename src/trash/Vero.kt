df35fce858dee2da6537f3a9f844c93e66eabcd9
leonidSpiri <2003.leonid2003@gmail.com>
2022-10-20 11:33:54 +0300

    cool shit

---
+++ src/trash/Vero.kt
@@ -0,0 +1,22 @@
+package trash
+
+fun main() {
+    val list = arrayOf(31, 42, 54, 74, 55, 66, 78, 82, 92)
+    val resList = arrayListOf<Int>()
+    var all = 0
+    for (i in list.indices) {
+        for (j in list.indices) {
+            if (list[i] == list[j]) continue
+            all++
+            val buff = "${list[i]}${list[j]}".toInt()
+            if (buff % 2 != 0)
+                resList.add(buff)
+        }
+    }
+    println("\nresult:\n")
+    println(resList.size)
+    println("--")
+    println(all)
+    println()
+    println(resList.toString())
+}
