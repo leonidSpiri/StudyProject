525b34fca7248806167d5732e7bb0577e745db67
leonidSpiri <2003.leonid2003@gmail.com>
2023-09-15 23:26:33 +0300

    cool shit

--- src/leetcode/LeetCode.kt
+++ src/leetcode/LeetCode.kt
@@ -6,6 +6,11 @@ private fun main() {
     //println(findTheDifferenceOfTwoArrays(intArrayOf(1, 2, 3, 3), intArrayOf(1, 1, 2, 2))) //3
     //println(minimumCostOfBuyingCandiesWithDiscount(intArrayOf(6, 5, 3, 4))) //4
     //println(countElementsWithStrictlySmallerAndGreaterElements(intArrayOf(-3, 3, 3, 90))) //5
+    //println(twoSum(intArrayOf(3, 3), 6).contentToString()) //6
+    //println(isPalindrome(11)) //7
+    //mergeSortedArray(nums1 = intArrayOf(1, 2, 3, 0, 0, 0), m = 3, nums2 = intArrayOf(2, 5, 6), n = 3) //8
+    //println(plusOne(intArrayOf(2, 3, 4, 9, 9, 8, 7, 9, 9)).contentToString()) //9
+    //println(addBinary("1010", "1011")) //10
 }
 
 private fun countHillsAndValleysInAnArray(nums: IntArray): Int {
@@ -94,3 +99,82 @@ private fun countElementsWithStrictlySmallerAndGreaterElements(nums: IntArray): 
     }
     return (buffArray.size - 2)
 }
+
+private fun twoSum(nums: IntArray, target: Int): IntArray {
+    /*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.*/
+    if (nums.size < 2) return nums
+    for (i in nums.indices) {
+        for (j in i + 1..nums.lastIndex) {
+            if (nums[i] + nums[j] == target) return intArrayOf(i, j)
+        }
+    }
+    return intArrayOf()
+}
+
+private fun isPalindrome(x: Int): Boolean {
+    //Given an integer x, return true if x is a palindrome, and false otherwise.
+    val array = x.toString().split("").filter { it.isNotEmpty() }
+    for (i in array.indices) {
+        if (array[i] != array[array.lastIndex - i]) return false
+    }
+    return true
+}
+
+private fun mergeSortedArray(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
+    /*You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.*/
+    for (i in 0 until n)
+        nums1[m + i] = nums2[i]
+    nums1.sort()
+    println(nums1.contentToString())
+}
+
+private fun plusOne(digits: IntArray): IntArray {
+    /*You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+    The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+    Increment the large integer by one and return the resulting array of digits.*/
+    var goToNext = true
+    var i = digits.lastIndex
+    while (goToNext) {
+        if (i < 0) {
+            val l = mutableListOf(1)
+            l.addAll(digits.toList())
+            return l.toIntArray()
+        }
+        if (digits[i] != 9) {
+            digits[i] = digits[i] + 1
+            goToNext = false
+        } else {
+            digits[i] = 0
+            i--
+        }
+    }
+    return digits
+}
+
+private fun addBinary(a: String, b: String): String {
+    //Given two binary strings a and b, return their sum as a binary string.
+    val list1 = a.reversed().split("").filter { it.isNotEmpty() }
+    val list2 = b.reversed().split("").filter { it.isNotEmpty() }
+    val longerArraySize = if (list1.size > list2.size) list1.size else list2.size
+    var pass = 0
+    var result = ""
+    for (i in 0 until longerArraySize) {
+        val sum = getNextBinaryValueFromList(i, list1) + getNextBinaryValueFromList(i, list2) + pass
+        pass = 0
+        if (sum > 1) {
+            pass = 1
+            result += sum - 2
+        } else {
+            result += sum.toString()
+        }
+    }
+    if (pass == 1) result += "1"
+    return result.reversed()
+}
+
+private fun getNextBinaryValueFromList(i: Int, list: List<String>) =
+    if (i <= list.lastIndex && list[i] == "1") 1 else 0
